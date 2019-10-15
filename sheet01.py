9cf63cd037114411e774427cd7e094fac0037eb7
laleh samadfam <laleh.samadfam@gmail.com>
2019-10-15 22:43:06 +0200

    problem 2 done

--- sheet01.py
+++ sheet01.py
@@ -44,53 +44,89 @@ def get_patch(img, patch_height, patch_width):
     height, width = img.shape[0], img.shape[1]
     row = random.randint(1, height - patch_height)
     col = random.randint(1, width - patch_width)
-    return img[row:row + patch_height, col + col + patch_width]
+    return img[row:row + patch_height, col : col + patch_width]
 
+def hist_eq(img):
+    width, height, size = img.shape[0], img.shape[1], img.size
+    #creating histogram of an image
+    hist = np.zeros(256)
+    for i in range(width):
+        for j in range(height):
+            hist[img[i,j]] += 1
+    #creating cumulitive histogram of image
+    cumulitive_hist = np.zeros(256)
+    cumulitive_hist[0] = hist[0]
+    for i in range(1, hist):
+        cumulitive_hist[i] = cumulitive_hist[i - 1] + hist[i]
+    #histogram equalize image
+    for i in range(width):
+        for j in range(height):
+            img[i,j] = 255 / size * cumulitive_hist[img[i,j]]
+    return img
+
+def max_err(src, res):
+    err = 0
+    for i in range(src.shape[0]):
+        for j in range(src.shape[1]):
+            if src[i,j] - res[i,j] > err:
+                err = src[i,j] - res[i,j]
+    return err
+#    =========================================================================
+img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)  # read image
+#    =========================================================================
 
 #    =========================================================================    
 #    ==================== Task 1 =================================
 #    =========================================================================    
-print('Task 1:');
 
-img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)  # read image
+ print('Task 1:');
+ # a
+ integral_img = my_integral(img)
+ cv.imshow('integral image', integral_img)
 
-# a
-integral_img = my_integral(img)
-cv.imshow('integral image', integral_img)
+ # b
+ # using sum of image values
+ mean_value_i = mean_integral_i(img)
 
-# b
-# using sum of image values
-mean_value_i = mean_integral_i(img)
+ # using cv.integral
+ mean_value_ii = mean_integral_ii(img)
 
-# using cv.integral
-mean_value_ii = mean_integral_ii(img)
+ # using integral image of our own
+ mean_value_iii = mean_integral_iii(img)
 
-# using integral image of our own
-mean_value_iii = mean_integral_iii(img)
+ # c
+ for i in range(10):
+     patch = get_patch(img,10, 10)
 
-# c
-for i in range(10):
-    patch = get_patch(img,100, 100)
+     t_start = time.process_time()
+     mean_integral_i(patch)
+     t_end = time.process_time()
+     print('patch', i, 'mean integral i process time = ' , (t_end - t_start))
 
-    t_start = time.process_time()
-    mean_integral_i(patch)
-    t_end = time.process_time()
-    print('processtime = ' + (t_end - t_start))
+     t_start = time.process_time()
+     mean_integral_ii(patch)
+     t_end = time.process_time()
+     print('patch', i, 'mean integral ii process time = ' , (t_end - t_start))
 
-    t_start = time.process_time()
-    mean_integral_ii(patch)
-    t_end = time.process_time()
-    print('processtime = ' + (t_end - t_start))
-
-    t_start = time.process_time()
-    mean_integral_iii(patch)
-    t_end = time.process_time()
-    print('processtime = ' + (t_end - t_start))
+     t_start = time.process_time()
+     mean_integral_iii(patch)
+     t_end = time.process_time()
+     print('patch', i, 'mean integral iii process time = ' , (t_end - t_start))
 
 #    =========================================================================
 #    ==================== Task 2 =================================
 #    =========================================================================    
 print('Task 2:');
+
+#a
+cv_img_hist_eq = cv.equalizeHist(img)
+cv.imshow('cv histogram equalized', cv_img_hist_eq)
+
+my_img_hist_eq = hist_eq(img)
+cv.imshow('my histogram qualized', my_img_hist_eq)
+
+print(max_err(cv_img_hist_eq, my_img_hist_eq))
+
 
 #    =========================================================================
 #    ==================== Task 4 =================================
