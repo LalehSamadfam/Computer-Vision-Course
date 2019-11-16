85cfaec127b48f6a513e371b12c031e41a5dd5e0
laleh samadfam <laleh.samadfam@gmail.com>
2019-11-16 17:34:12 +0100

    can't debug anymore

--- source codes/task01.py
+++ source codes/task01.py
@@ -23,7 +23,6 @@ def plot_snake(ax, V, fill='green', line='red', alpha=1, with_txt=False):
         for i, (x, y) in enumerate(V):
             ax.text(x, y, str(i))
 
-
 def load_data(fpath, radius):
     """
     :param fpath:
@@ -33,8 +32,8 @@ def load_data(fpath, radius):
     Im = cv2.imread(fpath, 0)
     h, w = Im.shape
     n = 20  # number of points
-    u = lambda i: radius * np.cos(i) + w / 2
-    v = lambda i: radius * np.sin(i) + h / 2
+    u = lambda i: radius * np.cos(i) + w / 2 - 1
+    v = lambda i: radius * np.sin(i) + h / 2 - 1
     V = np.array(
         [(u(i), v(i)) for i in np.linspace(0, 2 * np.pi, n + 1)][0:-1],
         'int32')
@@ -45,20 +44,32 @@ def load_data(fpath, radius):
 # ===========================================
 # RUNNING
 # ===========================================
+def energy(V, image, d):
+    grad_x, grad_y = np.gradient(image)
+    e = 0
+    for i in range(V.shape[0]):
+        e += unary_cost(4, V[i], grad_x, grad_y)
+    for i in range(V.shape[0] - 1):
+        e += binary_cost(V, i, i + 1, 4, 4, d)
+    e += binary_cost(V, V.shape[1] - 1, 0, 4, 4, d)
+    return e
 
-def stiffness(vertex, state, prev_state):
+
+def stiffness(vertex1, vertex2, state, prev_state):
     return 0
 
-def elasticity(vertices, vertex, state, prev_state, d):
+
+def elasticity(vertices, vertex1, vertex2, state, prev_state, d):
     x, y = get_state_position(state)
     x_p, y_p = get_state_position(prev_state)
-    cost = np.sqrt(((vertices[vertex][0] + x) - (vertices[vertex - 1][0] - x_p)) ** 2 +
-                   ((vertices[vertex][1] + y) - (vertices[vertex - 1][1] - y_p)) ** 2 - d)
+    cost = (np.sqrt(((vertices[vertex1][0] + x) - (vertices[vertex2][0] + x_p)) ** 2 +
+                    ((vertices[vertex1][1] + y) - (vertices[vertex2][1] + y_p)) ** 2) - d) ** 2
     return cost
 
 
-def binary_cost(vertices, vertex, state, prev_state, mean_dist): # internal cost
-    return stiffness(vertex, state, prev_state) + elasticity(vertices, vertex, state, prev_state, mean_dist)
+def binary_cost(vertices, vertex1, vertex2, state, prev_state, mean_dist):  # internal cost
+    return stiffness(vertex1, vertex2, state, prev_state) + elasticity(vertices, vertex1, vertex2, state, prev_state,
+                                                                       mean_dist)
 
 
 def get_state_position(state):
@@ -82,65 +93,60 @@ def get_state_position(state):
         return 1, 1
 
 
-def unary_cost(state, vertex, grad_x, grad_y): # external cost
+def unary_cost(state, vertex, grad_x, grad_y):  # external cost
     x, y = get_state_position(state)
-    #print(x,y)
     i = vertex[0] + x
     j = vertex[1] + y
-    #print(i, j)
-    return -1 * ((grad_x[i][j]) ** 2 + (grad_y[i][j]) ** 2)
+    if i >= grad_x.shape[0] or j >= grad_x.shape[1] :
+        return 1000
+    else:
+        return -1 * ((grad_x[i][j]) ** 2 + (grad_y[i][j]) ** 2)
 
 
 # FUNCTIONS
 def find_path(mat, vertices, image, mean_dist):
-    alpha = 1000
-    #grad_x, grad_y = np.gradient(image)
-    sobelx64f = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
-    abs_sobel64f = np.absolute(sobelx64f)
-    grad_y = np.uint8(abs_sobel64f)
-    sobelx64f = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
-    abs_sobel64f = np.absolute(sobelx64f)
-    grad_x = np.uint8(abs_sobel64f)
+    alpha = 150
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+    grad_x, grad_y = np.gradient(image)
     for i in range(mat.shape[0]):
-        mat[i][0] = unary_cost(i, vertices[0], grad_x, grad_y)
-    print('mat:')
-    print(mat)
+        mat[i][1] = alpha * binary_cost(vertices, 1, 0, i, 4, mean_dist) + unary_cost(i, vertices[1], grad_x, grad_y)
     min_paths = np.zeros((mat.shape[0], mat.shape[1]))
-    for j in range(1, mat.shape[1]):
+    for i in range(mat.shape[0]):
+        min_paths[i][1] = 4
+    for j in range(2, mat.shape[1] - 1):
         for i in range(mat.shape[0]):
-            costs = np.zeros(vertices.shape[0])
-            for k in range(costs.size):
-                costs[k] = binary_cost(vertices, j, i, k, mean_dist)
-            #input('new cost will be printed')
-            #print('i, j=', i, j)
-            #print('costs:')
-            #print(costs)
+            costs = np.zeros(mat.shape[0])
             unary = unary_cost(i, vertices[j], grad_x, grad_y)
-            #print('unary= ', unary)
-            mat[i][j] = alpha * np.min(costs) + unary
+            for k in range(costs.size):
+                binary = binary_cost(vertices, j, j - 1, i, k, mean_dist)
+                costs[k] = alpha * binary + mat[k][j - 1]
+
+            mat[i][j] = np.min(costs) + unary
             single_path = np.argmin(costs)
             min_paths[i][j] = single_path
-    #print('mat:')
-    #print(mat)
-    #print(min_paths)
+
+    for i in range(mat.shape[0]):
+        mat[i][mat.shape[1] - 1] = 100
+    costs = np.zeros(mat.shape[0])
+    for k in range(costs.size):
+        costs[k] = alpha * binary_cost(vertices, 0,  mat.shape[1] - 2, 4, k, mean_dist) + mat[k][mat.shape[1] - 2]
+
+    mat[4][mat.shape[1] - 1] = np.min(costs)  # + unary_cost(4, vertices[0], grad_x, grad_y)
+    single_path = np.argmin(costs)
+    min_paths[4][mat.shape[1] - 1] = single_path
     path = []
-    end_vertex = np.argmin(mat[:, mat.shape[1] - 1])
-    #print(end_vertex)
+    end_vertex = int(min_paths[4][mat.shape[1] - 1])
     path.append(end_vertex)
-    #print(path)
-    for j in range(min_paths.shape[1] - 1):
-       # print(end_vertex)
-        #print(min_paths.shape[1] - j - 1)
-        end_vertex = int(min_paths[end_vertex][min_paths.shape[1] - j - 1])
+    for j in range(min_paths.shape[1] - 2):
+        end_vertex = int(min_paths[end_vertex][min_paths.shape[1] - j - 2])
         path.append(end_vertex)
-    #print(path)
+    path = path[::-1]
     return path
 
 
 def update_cost(mat, vertices, image, mean_dist):
     fixed_vertex = np.random.randint(vertices.shape[0])
     rolled = np.roll(vertices, 2 * fixed_vertex)
-    print(rolled)
     rolled_path = find_path(mat, rolled, image, mean_dist)
     path = np.roll(rolled_path, -2 * fixed_vertex)
     return path
@@ -153,6 +159,16 @@ def update_vertices(old, new_states):
         old[i][1] = old[i][1] + y
     return old
 
+
+def mean_eu_dist(V):
+    mean = 0
+    for i in range(V.shape[0] - 1):
+        mean += np.sqrt((V[i][0] - V[i + 1][0]) ** 2 + (V[i][1] - V[i + 1][1]) ** 2)
+    mean += np.sqrt((V[0][0] - V[V.shape[0] - 1][0]) ** 2 + (V[0][1] - V[V.shape[0] - 1][1]) ** 2)
+    mean /= V.shape[0]
+    return mean
+
+
 def run(fpath, radius):
     """ run experiment
     :param fpath:
@@ -160,15 +176,13 @@ def run(fpath, radius):
     :return:
     """
     Im, V = load_data(fpath, radius)
-
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
     n_steps = 200
     states = 9
-    mean_dist = np.sqrt((V[0][0] - V[1][0]) ** 2 + (V[0][1] - V[1][1]) ** 2)
-
     for t in range(n_steps):
-        cost_mat = np.zeros((states, V.shape[0]))
+        mean_dist = mean_eu_dist(V)
+        cost_mat = np.zeros((states, V.shape[0] + 1))
         path = update_cost(cost_mat, V, Im, mean_dist)
         V = update_vertices(V, path)
         ax.clear()
@@ -176,7 +190,6 @@ def run(fpath, radius):
         ax.set_title('frame ' + str(t))
         plot_snake(ax, V)
         plt.pause(0.01)
-
     plt.pause(2)
 
 
