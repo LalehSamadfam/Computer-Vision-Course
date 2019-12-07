dd3309e3345e5e18a49f18216485d013e31e57c7
laleh samadfam <laleh.samadfam@gmail.com>
2019-12-07 22:49:26 +0100

    task 2 completed

--- source codes/Task_2_3.py
+++ source codes/Task_2_3.py
@@ -30,27 +30,21 @@ def compute_cov(data):
     return cov
 
 
-def pca(data, mean, k = 5):
+def pca(data, k = 5):
     vec = np.dot(data, np.transpose(data))
     u, v = np.linalg.eig(vec)
     u.sort(axis=0)
     u_k_r = u[-k - 1:-1]
     u_k = u_k_r[::-1]
-    u_k_m = np.zeros((u.shape[0], u_k.shape[0]))
-    v_k = np.zeros((k, k))
-    for i in range(k):
+    indx = np.where(u == u_k[0])
+    vec = v[:, indx[0]]
+    v_k = vec
+
+    for i in range(1, k):
         indx = np.where(u == u_k[i])
-        col = np.zeros((u_k_m.shape[0], 1))
-        for j in range(u_k_m.shape[0]):
-            if j == indx[0][0]:
-                col[j][0] = u_k[i].real
-                #print(u_k[i])
-            else:
-                col[j][0] = 0
-        u_k_m[:, i] = col[:, 0]
-        vector = v[indx]
-        v_k[i] = vector[0][0:5].real
-    return u_k_m, v_k
+        vec = v[:, indx[0]]
+        v_k = np.append(v_k, vec, axis=1)
+    return v_k.real
 
 def plot(ax, V, fill='green', line='red', alpha=1, with_txt=False):
     """ plots the snake onto a sub-plot
@@ -62,9 +56,10 @@ def plot(ax, V, fill='green', line='red', alpha=1, with_txt=False):
     :param with_txt: if True plot numbers as well
     :return:
     """
-    V_plt = np.append(V.reshape(-1), V[0,:]).reshape((-1, 2))
-    ax.plot(V_plt[:,0], V_plt[:,1], color=line, alpha=alpha)
-    ax.scatter(V[:,0], V[:,1], color=fill,
+
+    V_plt = np.append(V.reshape(-1), V[0, :]).reshape((-1, 2))
+    ax.plot(V_plt[:, 0], V_plt[:, 1], color=line, alpha=alpha)
+    ax.scatter(V[:, 0], V[:, 1], color=fill,
                edgecolors='black',
                linewidth=2, s=50, alpha=alpha)
     if with_txt:
@@ -75,20 +70,25 @@ def plot(ax, V, fill='green', line='red', alpha=1, with_txt=False):
 def task_2():
     size, train_data = read_train_data('data/hands_aligned_train.txt.new')
     mu = compute_mean(train_data)
-    cov = compute_cov(train_data)
-    phi = np.random.rand(size[0], size[1])  # array of K D-dimensional phis
-    u_k, v_k = pca(train_data, mu)
-    phi = np.dot(u_k, np.sqrt(v_k))
+    v_k = pca(train_data)
+    phi = v_k
     shape = np.zeros((train_data.shape[0], 1))
     h = [-0.4, -0.2, 0.0, 0.2, 0.4]
     var = np.dot(phi, h)
     for i in range(train_data.shape[0]):
         shape[i] = mu[i] + var[i]
+
+# Visualization
+    w = np.zeros((int(shape.shape[0]/2), 2))
+    for i in range(w.shape[0]):
+        w[i][0] = shape[i]
+        w[i][1] = shape[56 + i]
+
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
-    plot(ax, shape)
+    ax.clear()
+    plot(ax, w)
     plt.show()
-    plt.pause(0.01)
 
 
 def task_3():
